ae22d62dc2795957aedc2342c6a4c9da6953c478
hankai17 <hankai17@126.com>
2026-08-11 23:30:10 +0800

    add test

--- build/cmd.sh
+++ build/cmd.sh
@@ -1,2 +1,6 @@
 ./a.out "SELECT name,age FROM user WHERE id = (SELECT id FROM admin)"
 ./yanshi sql.txt --dump-automaton --dump-assoc > 1.log
+
+./a.out "SELECT*FROMA"
+grep elapse  | sort -k7,7nr | less
+
