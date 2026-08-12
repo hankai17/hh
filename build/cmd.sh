e0b3ed777ae8dc32d455e8bb5f79f6c791e9943a
hankai17 <hankai17@126.com>
2026-08-13 02:14:27 +0800

    sql.txt base

--- build/cmd.sh
+++ build/cmd.sh
@@ -4,3 +4,14 @@
 ./a.out "SELECT*FROMA"
 grep elapse  | sort -k7,7nr | less
 
+SELECT *
+FROM t
+WHERE
+    -a + b * c / d % e || f -> g ->> h << i >> j & k | l < m
+    AND
+    a <= n
+    AND
+    b > o
+    AND
+    c >= p;
+
